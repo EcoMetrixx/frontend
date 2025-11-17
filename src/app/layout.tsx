81697fc15a1afc9cfc2447948abe5fb2432649b9
faryd <mirandafaryd@gmail.com>
2025-11-16 22:05:37 -0500

    🌱 Feat: metadata added
    title and description added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,18 @@
 import Providers from "@/core/providers"
 import { Toaster } from "react-hot-toast"
 
+export const metadata = {
+  title: "Ecometrix",
+  description: "Web destianda a inmobiliaras que financian hogares a traves de los bonos Mi Vivienda y Techo Propio",
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="es" suppressHydrationWarning>
       <body>
         <Providers>
           { children }
